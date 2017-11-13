6321a549ce92417e036eda8e3e13575ca09635f0
David Costa <davidcostadev@gmail.com>
2017-11-13 16:20:33 -0300

    needs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router } from 'react-router';
-import { Route, Switch } from 'react-router-dom' 
+import { Router, IndexRoute } from 'react-router';
+import { Route, Switch, Link } from 'react-router-dom' 
 import store from './store'
 
 
@@ -15,16 +15,31 @@ import Private from './pages/Private'
 
 import createHistory from 'history/createBrowserHistory'
 
+const PrimaryLayout = props => (
+  <div className="primary-layout">
+    <header>
+      <Link to="/">Home</Link> - 
+      <Link to="/private">Private</Link> - 
+      <Link to="/about">About</Link>
+    </header>
+    <main>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/about" component={About} />
+      <Route path="/private" component={Private} />
+    </main>
+  </div>
+)
+
+
 render(
   <Provider store={store}>
     <Router history={createHistory()}>
       <div>
         header
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/login" component={Login} />
-          <Route path="/private" component={Private} />
+          <PrimaryLayout />
+          
           <Route component={Page404} />
         </Switch> 
         footer
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom'
 
 export default () => (
   <div>
-    <Link to="/">Home</Link> - 
-    <Link to="/private">Private</Link> - 
-    <Link to="/about">About</Link>
     <h1>Login</h1>    
     Email<br />
     <input type="email" />
